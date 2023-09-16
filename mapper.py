b4480eada09865caa880a9bdf3bf4bf3fa20226a
@askf2 <askf2@mst.edu>
2023-09-16 15:48:30 -0500

    made mapereduce ready

--- mapper.py
+++ mapper.py
@@ -1,33 +1,32 @@
-import fileinput
+import sys
 
-with fileinput.input(files=('input.txt')) as f:
-    key_value_pairs = []
-    for line in f:
-        person, friends = line.split('   ')
-        friends_list = friends.split(',')
-        friends_list[-1] = friends_list[-1].strip()
-        # friends_list = set(friends_list)
-        for friend in friends_list:
-            key = tuple(sorted((person, friend)))
-            value = friends_list
-            key_value_pairs.append((key, tuple(value)))
-    # print(key_value_pairs)
-    # print('----------------')
+key_value_pairs = []
 
-    pairs1 = []
-    keys = []
-    for i in range(0, len(key_value_pairs)):
-        if key_value_pairs[i][0] not in keys:
-            key = key_value_pairs[i][0]
-            values = [key_value_pairs[i][1]]
-            for j in range(i+1, len(key_value_pairs)):
-                if key_value_pairs[i][0] == key_value_pairs[j][0]:
-                    values.append(key_value_pairs[j][1])
-            pairs1.append([key, values])
-            keys.append(key)
+for line in sys.stdin:
+    person, friends = line.split('   ')
+    friends_list = friends.split(',')
+    friends_list[-1] = friends_list[-1].strip()
+    for friend in friends_list:
+        key = tuple(sorted((person, friend)))
+        value = friends_list
+        key_value_pairs.append((key, tuple(value)))
+# print(key_value_pairs)
+# print('----------------')
 
-    for pair in pairs1:
-        out = str(pair[0]) + '   '
-        for val in pair[1]:
-            out += str(val) + ';'
-        print(out)
+pairs1 = []
+keys = []
+for i in range(0, len(key_value_pairs)):
+    if key_value_pairs[i][0] not in keys:
+        key = key_value_pairs[i][0]
+        values = [key_value_pairs[i][1]]
+        for j in range(i+1, len(key_value_pairs)):
+            if key_value_pairs[i][0] == key_value_pairs[j][0]:
+                values.append(key_value_pairs[j][1])
+        pairs1.append([key, values])
+        keys.append(key)
+
+for pair in pairs1:
+    out = str(pair[0]) + '   '
+    for val in pair[1]:
+        out += str(val) + ';'
+    print(out)
--- reducer.py
+++ reducer.py
@@ -1,13 +1,12 @@
-import fileinput
+import sys
 import re
 
-with fileinput.input(files=('secondInput.txt')) as f:
-    for line in f:
-        key, value = line.split('   ')
-        values = value.split(';')[:-1]
-        numbers = set([int(num) for num in re.findall(r'\d+', values[0])])
-        common_friends = numbers
-        for i in range(1, len(values)):
-            numbers = set([int(num) for num in re.findall(r'\d+', values[i])])
-            common_friends = common_friends.intersection(numbers)
-        print(key, '   ', tuple(common_friends))
+for line in sys.stdin:
+    key, value = line.split('   ')
+    values = value.split(';')[:-1]
+    numbers = set([int(num) for num in re.findall(r'\d+', values[0])])
+    common_friends = numbers
+    for i in range(1, len(values)):
+        numbers = set([int(num) for num in re.findall(r'\d+', values[i])])
+        common_friends = common_friends.intersection(numbers)
+    print(key, '   ', tuple(common_friends))
